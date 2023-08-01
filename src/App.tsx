25b261a288f461eeab2d37e5973490c1a1bfe3d9
sameer0075 <ismail.aijaz@simplextechnologysolutions@gmail.com>
2023-08-01 21:54:37 +0500

    redux toolkit setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AppFooter from './Partials/footer';
 import AppHeader from './Partials/header';
 import { PublicRoutes, PrivateRoutes } from './routes';
 import { RouteConfig } from './routes/Interface/interface';
-
+import { store } from './redux/store';
 function App() {
   const token = sessionStorage.getItem('token');
   return (
     <div className="App">
       {token && <AppHeader />}
+      <Provider store={store}>
+      <ToastContainer />
       <BrowserRouter>
         <Routes>
             {!token
@@ -21,6 +26,7 @@ function App() {
                 ))}
         </Routes>
       </BrowserRouter>
+      </Provider>
       {token && <AppFooter />}
     </div>
   );
